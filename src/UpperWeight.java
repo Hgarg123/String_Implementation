9a68e890b24bb71f45bcb306bbdfd91151d05625
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-03 22:05:10 +0530

    initial sum=0

--- src/UpperWeight.java
+++ src/UpperWeight.java
@@ -4,8 +4,12 @@ public class UpperWeight {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String str = obj.nextLine();
+        int sum = 0;
         for (int i = 0; i < str.length(); i++) {
             char character = str.charAt(i);
+            if (character >= 'A' && character <= 'Z') {
+
+            }
 
         }
     }
