c02aaaaf062b5f7b42058bd5cddb3d9a7d31b954
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-03 21:43:46 +0530

    input string by user

--- src/ModifyString.java
+++ src/ModifyString.java
@@ -4,6 +4,14 @@ public class ModifyString {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String str = obj.nextLine();
+        String str2;
 
+        for (int i = 0; i < str.length(); i++) {
+            str2 = str.replaceFirst("[aeiou]", "*");
+            str2 = str2.replaceFirst("[aeiou]", "^");
+            str2 = str2.replaceFirst("[aeiou]", "!");
+
+            System.out.println(str2);
+        }
     }
 }
---
+++ src/Uppercase.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Uppercase {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String str = obj.nextLine();
+    }
+}
