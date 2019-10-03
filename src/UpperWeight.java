88fd8f21245349e9d247ca38239f39433b8ee787
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-03 22:00:38 +0530

    input string by user

---
+++ src/UpperWeight.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class UpperWeight {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String str = obj.nextLine();
+    }
+}
