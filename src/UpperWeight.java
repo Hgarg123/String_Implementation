a9b1671105c1c87a9a184e22e66e31d16fe192f5
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-03 22:02:04 +0530

    start loop

--- src/UpperWeight.java
+++ src/UpperWeight.java
@@ -4,5 +4,9 @@ public class UpperWeight {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String str = obj.nextLine();
+        for (int i = 0; i < str.length(); i++) {
+
+
+        }
     }
 }
