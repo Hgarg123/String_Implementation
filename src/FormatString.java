bc88ac41f088bf4e0cce992778e2346828d2cf6e
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-03 20:30:32 +0530

    create loop

--- src/FormatString.java
+++ src/FormatString.java
@@ -4,7 +4,10 @@ public class FormatString {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String str = obj.nextLine();
-
+        System.out.print("index:\t");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(i + "\t");
+        }
 
     }
 }
