46e2a0db114d910cf3c2201be8c31384af0414ab
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-03 20:29:28 +0530

    input version

--- src/FormatString.java
+++ src/FormatString.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class FormatString {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String str = obj.nextLine();
+
+
+    }
 }
