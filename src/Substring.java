9d6333e7257327556cd9d9cf8595deb6853b7831
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-03 20:40:37 +0530

    create substring method

--- src/Substring.java
+++ src/Substring.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
 public class Substring {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String substring = obj.nextLine();
+        int index = obj.nextInt();
+        String substring1 = substring.substring(index);
 
+
+    }
 }
+
