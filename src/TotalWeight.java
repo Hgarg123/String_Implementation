b90231bc5ab034c4fd71ce1998bd7e042be6a300
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-03 21:54:05 +0530

    add ascii to its previous value

--- src/TotalWeight.java
+++ src/TotalWeight.java
@@ -7,7 +7,8 @@ public class TotalWeight {
         for (int i = 0; i < str.length(); i++) {
             char character = str.charAt(i);
             int ascii = (int) character;
-
+            sum = sum + ascii;
         }
+
     }
 }
