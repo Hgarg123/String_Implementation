336b3f96bb4f0b0d106d1eb60f238b98622d504e
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-03 21:48:34 +0530

    input string

--- src/TotalWeight.java
+++ src/TotalWeight.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
 public class TotalWeight {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String str = obj.nextLine();
 
+
+    }
 }
