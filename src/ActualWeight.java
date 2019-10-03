71b02facf9343e4c0ee1114ecec9e160e8cde8a7
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-03 22:13:49 +0530

    input string

---
+++ src/ActualWeight.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class ActualWeight {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String str = obj.nextLine();
+    }
