4cabab624bc122d7b6b81ad80a61af53db8e1331
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-03 20:54:47 +0530

    init commit

---
+++ src/ModifyString.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ModifyString {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String str = obj.nextLine();
+
+    }
+}
