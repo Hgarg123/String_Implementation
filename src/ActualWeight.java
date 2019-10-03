6a65255b6d52edc1254e91af25a9206ab7ad7078
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-03 22:18:40 +0530

    add ascii in previous sum

--- src/ActualWeight.java
+++ src/ActualWeight.java
@@ -4,12 +4,13 @@ public class ActualWeight {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String str = obj.nextLine();
-        int sum = 0;
+        int sumOfUpperWeight = 0;
         for (int i = 0; i < str.length(); i++) {
             char character = str.charAt(i);
             if (character >= 'A' && character <= 'Z') {
-
+                sumOfUpperWeight = sumOfUpperWeight + (int) character;
             }
         }
+
     }
 }
