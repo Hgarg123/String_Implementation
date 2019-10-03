9957d144dd3a5d695086c27eff786af890606716
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-03 21:51:13 +0530

    create charAt method

--- src/TotalWeight.java
+++ src/TotalWeight.java
@@ -5,7 +5,7 @@ public class TotalWeight {
         String str = obj.nextLine();
         int sum = 0;
         for (int i = 0; i < str.length(); i++) {
-
+            char character = str.charAt(i);
 
         }
     }
