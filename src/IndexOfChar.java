8a2404c7012c76c524dcd5480e099410d3b85da1
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-04 20:44:03 +0530

    input string

--- src/IndexOfChar.java
+++ src/IndexOfChar.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
 public class IndexOfChar {
-    
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String str = obj.nextLine();
+    }
+
 }
