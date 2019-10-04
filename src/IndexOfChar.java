bf237366acd410ed6f24670b0fa2d10ce95ab74b
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2019-10-04 20:51:06 +0530

    input a char from user

--- src/IndexOfChar.java
+++ src/IndexOfChar.java
@@ -3,6 +3,8 @@ public class IndexOfChar {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String str = obj.nextLine();
+        char character = obj.next().charAt(0);
+
     }
 
 }
