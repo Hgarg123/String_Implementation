ec68614f14576ab693e96c2f9c19088e1b15f26f
Harshit Garg <54099867+Hgarg123@users.noreply.github.com>
2019-10-05 21:26:15 +0530

    Delete IndexAtNewLine.java

--- src/IndexAtNewLine.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class IndexAtNewLine {
-    public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        String str = obj.nextLine();
-}
